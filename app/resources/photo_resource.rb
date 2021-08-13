587bdbc2b48c06bbba043c4480b90c6c67580db8
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-13 10:55:34 +0000

    Generate graphiti resources for photos

---
+++ app/resources/photo_resource.rb
@@ -0,0 +1,14 @@
+class PhotoResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :caption, :string
+  attribute :image, :string
+  attribute :owner_id, :integer
+  attribute :location, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
