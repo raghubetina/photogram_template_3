7b53601b614824edb56104431af612b1ea1e9d38
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-13 10:54:50 +0000

    Only like user can modify like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :current_user_must_be_like_user, only: [:edit, :update, :destroy] 
+
   before_action :set_like, only: [:show, :edit, :update, :destroy]
 
   # GET /likes
@@ -57,6 +59,14 @@ class LikesController < ApplicationController
 
 
   private
+
+  def current_user_must_be_like_user
+    set_like
+    unless current_user == @like.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_like
       @like = Like.find(params[:id])
